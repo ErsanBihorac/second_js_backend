97ada60d24dba2b20d763a0cb468c4c818d8d41e
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-04-23 02:56:21 +0200

    save

---
+++ output.js
@@ -0,0 +1,10 @@
+const x = "1";
+const y = "2";
+
+// console.log(x, y);
+
+// %s format variable to string
+//%d for number
+//%i for integer
+//%o object
+// console.log("I am %s and my age is %d", "Ersan", 25);
