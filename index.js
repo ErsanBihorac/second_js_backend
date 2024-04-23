01ebbfe940af038fe3093e6843319834496301ee
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-04-23 02:43:30 +0200

    current state

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('this is node js tutorial');
+console.log('this is the first tutorial on nodejs');
+console.log('we will cover nodejs with command line');
+// process.exit(1);
+// process.exitCode = 0;
