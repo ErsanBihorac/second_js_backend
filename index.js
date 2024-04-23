c10f5abfdb3237faf623f45237f5bb3e169e5e2a
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-04-24 00:38:45 +0200

    covered NPM tool, how to manage all dependecencies, exports functionality and how to console log objects

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-console.log('this is node js tutorial');
-console.log('this is the first tutorial on nodejs');
-console.log('we will cover nodejs with command line');
+// console.log('this is node js tutorial');
+// console.log('this is the first tutorial on nodejs');
+// console.log('we will cover nodejs with command line');
 // process.exit(1);
 // process.exitCode = 0;
+
+// const _ = require("lodash");
+// const arr = [1, 4, 6, 8];
+// console.log(_.chunk(arr));
+
+// const cowsay = require('cowsay');
+// console.log(cowsay.say({
+//     text: "I am learning NPM modules",
+//     e: "00",
+//     t: "U"
+// }));
+
+const { tesla, ford } = require("./car");
+console.log(JSON.stringify(tesla, null, 3));
+console.log(JSON.stringify(ford, undefined, 2));
